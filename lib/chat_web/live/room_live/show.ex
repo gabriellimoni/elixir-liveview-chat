bcd58f6e4e41e3446160819fc8ab6360b573c478
Gabriel Limoni <gabrielblimoni@gmail.com>
2022-07-25 15:19:39 -0300

    feat: show online users

--- lib/chat_web/live/room_live/show.ex
+++ lib/chat_web/live/room_live/show.ex
@@ -2,10 +2,11 @@ defmodule ChatWeb.RoomLive.Show do
   use ChatWeb, :live_view
 
   alias Chat.ChatRoom
+  alias Phoenix.PubSub
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(:username, nil) |> assign(:users, nil)}
   end
 
   @impl true
@@ -13,9 +14,53 @@ defmodule ChatWeb.RoomLive.Show do
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:room, ChatRoom.get_room!(id))}
+     |> assign(:room, ChatRoom.get_room!(id))
+     |> assign(:room_id, id)}
   end
 
+  @impl true
+  def handle_event("username", %{"user" => %{"username" => username}}, socket) do
+    room_id = socket.assigns.room_id
+    ChatWeb.Presence.track(self(), room_id, socket.id, %{
+      username: username,
+    })
+    ChatWeb.Endpoint.subscribe(room_id)
+    PubSub.subscribe(Chat.PubSub, room_id)
+
+    {:ok, users} = get_users(room_id)
+
+    {:noreply,
+     socket
+     |> assign(:username, username)
+     |> assign(:users, users)}
+  end
+
+  @impl true
+  def handle_event("message", %{"message" => %{"content" => content}}, socket) do
+    IO.puts content
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "presence_diff"}, socket) do
+    {:ok, users} = get_users(socket.assigns.room_id)
+    {:noreply, socket |> assign(:users, users)}
+  end
+
+  defp get_users(room_id) do
+    users = room_id
+      |> ChatWeb.Presence.list()
+      |> Map.values()
+      |> Enum.map(fn presence ->
+        data = presence
+          |> Map.get(:metas)
+          |> List.first()
+        %{
+          username: data.username
+        }
+      end)
+    {:ok, users}
+  end
   defp page_title(:show), do: "Show Room"
   defp page_title(:edit), do: "Edit Room"
 end
